9f230e00600d7d0803b48fa39f9ca164e60459a7
Lucas Paulo <lucapaulodev@gmail.com>
2023-10-01 03:40:38 -0300

    Adding detail field to the Todo DTO.

--- src/todo/dto/create-todo.input.ts
+++ src/todo/dto/create-todo.input.ts
@@ -1,7 +1,10 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
+import { InputType, Field } from '@nestjs/graphql';
 
 @InputType()
 export class CreateTodoInput {
   @Field()
   title: string
+
+  @Field({ nullable: true })
+  detail?: string
 }
