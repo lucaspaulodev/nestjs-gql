09e2431c2d48dc50071885c3e57e18834682c375
Lucas Paulo <lucapaulodev@gmail.com>
2023-10-04 15:38:03 -0300

    Removing id from UpdateTodoInput..

--- src/todo/models/update-todo.input.ts
+++ src/todo/models/update-todo.input.ts
@@ -3,9 +3,6 @@ import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 
 @InputType()
 export class UpdateTodoInput extends PartialType(CreateTodoInput) {
-  @Field(() => Int)
-  id: number;
-
   @Field()
   title: string
 
