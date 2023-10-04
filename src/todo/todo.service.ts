38d56bf9f544b7062cbf59db2874642f3e55a0b0
Lucas Paulo <lucaspaulodev@gmail.com>
2023-10-04 15:40:05 -0300

    Adding types to todo.service methods.

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -3,12 +3,13 @@ import { CreateTodoInput } from './models/create-todo.input';
 import { UpdateTodoInput } from './models/update-todo.input';
 import { PrismaService } from '../prisma/prisma.service';
 import { CursorPaginationArgs } from '../todo/models/cursor-pagination-args.input';
+import { Todo, TodoConnection, TodoEdge } from './models/todo.model';
 
 @Injectable()
 export class TodoService {
   constructor(private prisma: PrismaService) {}
   
-  async create(createTodoInput: CreateTodoInput) {
+  async create(createTodoInput: CreateTodoInput): Promise<Todo> {
     return await this.prisma.todo.create({
       data: {
         title: createTodoInput.title,
@@ -17,16 +18,16 @@ export class TodoService {
     });
   }
   
-  async findWithPagination({ first, after }: CursorPaginationArgs) {
+  async findWithPagination({ first, after }: CursorPaginationArgs): Promise<TodoConnection> {
     const take = first + 1; // Fetch one more record than requested to determine hasNextPage
 
-    const todos = await this.prisma.todo.findMany({
+    const todos: Todo[] = await this.prisma.todo.findMany({
       take,
       cursor: after ? { id: after }  : undefined ,
       orderBy: {created_at: 'desc'}
     });
 
-    const edges = todos.slice(0, first).map((todo) => {
+    const edges: TodoEdge[] = todos.slice(0, first).map((todo) => {
       return {
         cursor: todo.id,
         node: todo,
@@ -45,35 +46,36 @@ export class TodoService {
     };
   }
 
-  async findAll() {
+  async findAll(): Promise<Todo[]> {
     return await this.prisma.todo.findMany();
   }
 
-  async findOne(id: number) {
+  async findOne(id: number): Promise<Todo> {
     return await this.prisma.todo.findUnique({where: { id }});
   }
 
-  async update(id: number, updateTodoInput: UpdateTodoInput) {
-    try{
-      return await this.prisma.todo.update({
+  async update(id: number, updateTodoInput: UpdateTodoInput): Promise<Todo> {
+    try {
+      const todo = await this.prisma.todo.update({
         where: { id },
         data: {
           title: updateTodoInput.title,
           detail: updateTodoInput.detail
         }
-      });
+      })
+
+      return todo;
     } catch (error) {
       throw new NotFoundException()
     }
     
   }
 
-  async remove(id: number) {
+  async remove(id: number): Promise<Todo> {
     try{
       return await this.prisma.todo.delete({where: { id }});
     } catch (error) {
       throw new NotFoundException()
     }
-    
   }
 }
