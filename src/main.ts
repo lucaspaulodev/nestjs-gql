c48a011a8a26fb8e9f4700fd659da9677d51af24
Lucas Paulo <lucaspaulodev@gmail.com>
2023-10-06 04:07:11 -0300

    Updating CORS configs.

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors()
+  app.enableCors({
+    origin: "*",
+    methods: 'GET, PUT, POST, DELETE',
+    allowedHeaders: 'Content-Type, Authorization'
+  })
   app.enableShutdownHooks();
 
   await app.listen(3333);
